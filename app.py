ec77beb84e7bc7250a0b01454bfb52b085772e4b
nandanajp1105 <nandanajp1105@gmail.com>
2026-04-29 11:35:14 +0530

    deployment fix

--- app.py
+++ app.py
@@ -87,4 +87,7 @@ def best_time():
 
 # -------- RUN --------
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
